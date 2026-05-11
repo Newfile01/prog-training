32d8922e042c995140ad090d16a3919f179f5034
Newfile01 <nicolas.morel.01000@gmail.com>
2026-05-11 10:10:25 +0200

    Environment setup README.md + install.sh added

---
+++ golang/go-env_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+# Download and verify checksum
+curl -LO https://go.dev/dl/go1.26.3.linux-amd64.tar.gz
+if [ "$(sha256sum go1.26.3.linux-amd64.tar.gz | cut -d' ' -f1)" = "2b2cfc7148493da5e73981bffbf3353af381d5f93e789c82c79aff64962eb556" ]; then
+    echo "✅ Valid file"
+else
+    echo "❌ Invalid checksum"
+    exit 1
+fi
+
+# Remove old & Download new Go compilator
+sudo rm -rf /usr/local/go
+sudo mv go1.26.3.linux-amd64.tar.gz /usr/local/go
+# Test
+if [ -f "/usr/localgo1.26.3.linux-amd64.tar.gz" ]; then 
+    echo "✅ File downloaded"
+else 
+    echo "❌ File missing"
+    exit 1
+fi
+
+sudo tar -C /usr/local -xzf /usr/localgo1.26.3.linux-amd64.tar.gz
+
+
+
+# Add to $PATH (user), add to $HOME/.profile or /etc/profile for wide
+export PATH=$PATH:/usr/local/go/bin
+# Test
+command -v go >/dev/null 2>&1 && echo "✅ Go added to \$PATH"
+
+# Test
+go version >/dev/null 2>&1 && echo "✅ Go OK" || echo "❌ Go absent"
